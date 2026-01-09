43a46b74815f895a25a32bf0495fb097b6bc6245
tomrcc <tom@cloudcannon.com>
2026-01-09 14:19:01 +1300

    Add env variable to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig, passthroughImageService } from "astro/config";
+import { loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import editableRegions from "@cloudcannon/editable-regions/astro-integration";
+const { STAGING } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [mdx(), sitemap(), icon(), editableRegions()],
+  integrations: [mdx(), sitemap(), icon(), STAGING && editableRegions()],
   vite: {
     plugins: [tailwindcss()],
   },
