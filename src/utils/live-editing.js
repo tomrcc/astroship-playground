13028a6259d8d9a4fac1a6b7b137757fdfa1f931
tomrcc <tom@cloudcannon.com>
2026-01-14 15:31:53 +1300

    Register grid component

--- src/utils/live-editing.js
+++ src/utils/live-editing.js
@@ -3,8 +3,10 @@ import Cta from "@/components/cta.astro";
 import Features from "@/components/features.astro";
 import Hero from "@/components/hero.astro";
 import Logos from "@/components/logos.astro";
+import Grid from "@/components/grid.astro"
 
 registerAstroComponent("cta", Cta);
 registerAstroComponent("features", Features);
 registerAstroComponent("hero", Hero);
 registerAstroComponent("logos", Logos);
+registerAstroComponent("grid", Grid);
